3abcc6b673232fc3eef402a4e43c8c678c827994
Bruno Vitte <bruno_vitte@hotmail.com>
2021-08-26 07:58:24 -0500

    First commit Creando los componentes mínimos del proyecto

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# Por buena práctica hay que indicar la versión requerida
+gem 'ruby2d'
---
+++ src/model/state.rb
@@ -0,0 +1,20 @@
+# Respresentación del estado del juego
+
+module Model
+   
+    class Coord < Struct.new(:row, :col)
+    end
+
+    class Food < Coord
+    end
+
+    class Snake < Struct.new(:positions)
+    end
+
+    class Grid < Struct.new(:rows, :cols)
+    end
+
+    class State < Struct.new(:snake, :food, :grid)
+    end
+    
+end
